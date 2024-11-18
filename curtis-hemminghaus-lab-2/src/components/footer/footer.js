a74d565bc27a9adb6777abce8c20c966b76fcf0e
CurtisHemm <146120781+CurtisHemm@users.noreply.github.com>
2024-11-18 18:16:05 -0500

    Removed MUI Navbar
    - Removed everything involving the MUI because of an installation error that wouldn't resolve itself

---
+++ curtis-hemminghaus-lab-2/src/components/footer/footer.js
@@ -0,0 +1,11 @@
+
+
+
+
+export default function Footer(){
+    return (
+        <>
+        <p>&copy; 2024 Curtis</p>
+        </>
+    )
+}
---
+++ curtis-hemminghaus-lab-2/src/components/header/header.js
@@ -0,0 +1,24 @@
+
+
+
+function Header(props) {
+  return (
+    <>
+    <h1>Header - Navbar</h1>
+    </>
+  )
+};
+
+export default Header;
+
+
+
+// export default function Header() {
+//     return (
+//         <>
+//         <h1>This is my header</h1>
+//         </>
+//     )
+// }
+
+
