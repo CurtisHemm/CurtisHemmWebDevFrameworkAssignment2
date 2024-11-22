5ca55b05e2c08132fcfae5826a996ee0b97aef61
CurtisHemm <146120781+CurtisHemm@users.noreply.github.com>
2024-11-22 04:23:09 -0500

    Rough Navbar
    - Added start of navbar with some CSS

--- curtis-hemminghaus-lab-2/src/components/header/header.js
+++ curtis-hemminghaus-lab-2/src/components/header/header.js
@@ -4,7 +4,18 @@
 function Header(props) {
   return (
     <>
-    <h1>Header - Navbar</h1>
+    <header class="header">
+      <h1 class="title">Header - Navbar</h1>
+      <nav class="nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/genre">Liked Genres</a></li>
+          <li><a href="/services">Favorite Genres</a></li>
+          <li><a href="/contact">Liked Games</a></li>
+          <li><a href="/contact">Favorite Games</a></li>
+        </ul>
+      </nav>
+    </header>
     </>
   )
 };
